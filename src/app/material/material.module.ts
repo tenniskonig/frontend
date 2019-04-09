563bfe191415f3232cabff318c4ab5a5ac348fea
tf98 <tobiasfritz@outlook.de>
2019-04-09 18:59:01 +0200

    Add Angular Material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {MatButtonModule, MatCheckboxModule, MatToolbarModule} from '@angular/material';
+
+@NgModule({
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
+})
+export class CustomMaterialModule { }
