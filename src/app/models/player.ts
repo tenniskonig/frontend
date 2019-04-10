b7efac2f80872776a7f07734cab5d83c3d74add2
tf98 <tobiasfritz@outlook.de>
2019-04-10 20:53:51 +0200

    Change player to user

--- src/app/models/player.ts
+++
@@ -1,5 +0,0 @@
-export class Player {
-  id: number;
-  vorname: string;
-  nachname: string;
-}
---
+++ src/app/models/user.ts
@@ -0,0 +1,7 @@
+export class User {
+  id: number;
+  vorname: string;
+  nachname: string;
+  geschlechtW: boolean;
+  admin: boolean;
+}
