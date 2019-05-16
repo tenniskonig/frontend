d05f4e3752736b6e22a1ca1dc7f598e5514dedd0
DMarvin Kubik <marvin@fam-kubik.de>
2019-05-16 15:06:16 +0200

    Add custom HTTP Interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,11 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {HighscoreComponent} from './highscore/highscore.component';
 import {MatchHistoryComponent} from './match-history/match-history.component';
 import {JwtModule} from '@auth0/angular-jwt';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {LoginComponent} from './login/login.component';
 import {RulesComponent} from './rules/rules.component';
 import {MAT_DATE_LOCALE} from '@angular/material';
+import {JwtHttpInterceptor} from './services/httpInterceptor';
 
 @NgModule({
   declarations: [
@@ -39,14 +40,18 @@ import {MAT_DATE_LOCALE} from '@angular/material';
           return localStorage.getItem('access_token');
         },
         // alles bei dem header ersetzt werden soll
-        whitelistedDomains: ['http://localhost:8080'],
+        whitelistedDomains: new Array(new RegExp('^null$'))
+
+  ,
         // alles  bei dem header nicht ersetzt werden soll
-        blacklistedRoutes: ['http://tennisClientID:KADFgni46agPQ@localhost:8080/oauth/token']
+        blacklistedRoutes: ['tennisClientID:KADFgni46agPQ@localhost:8080/oauth/token'],
+        headerName: 'Authorization'
       }
     }),
   ],
   providers: [
     {provide: MAT_DATE_LOCALE, useValue: 'de-DE'},
+    { provide: HTTP_INTERCEPTORS, useClass: JwtHttpInterceptor, multi: true },
   ],
   bootstrap: [AppComponent]
 })
