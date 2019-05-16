e1e09fe40bf8bd58b33b3e42b7fcd5225ab3ad84
Marvin Kubik <marvin@fam-kubik.de>
2019-05-16 20:50:45 +0200

    Add Authorization header to user service requests

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {User} from '../models/user';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {ConfigService} from './config.service';
 
 @Injectable({
@@ -12,11 +12,21 @@ export class UserService {
   constructor(private http: HttpClient) {
   }
 
+  private getHttpOptions() {
+    return {
+      headers: new HttpHeaders({
+        Authorization: 'Basic ' + window.btoa(ConfigService.clientID + ':' + ConfigService.secret)
+      })
+    };
+  }
+
   getAllUsers(): Observable<User[]> {
-    return this.http.get<User[]>(ConfigService.baseURL + 'api/player');
+    const httpOptions = this.getHttpOptions();
+    return this.http.get<User[]>(ConfigService.baseURL + '/api/player', httpOptions);
   }
 
   getUserByID(id: number): Observable<User> {
-    return this.http.get<User>(ConfigService.baseURL + 'api/player/' + id);
+    const httpOptions = this.getHttpOptions();
+    return this.http.get<User>(ConfigService.baseURL + '/api/player/' + id, httpOptions);
   }
 }
