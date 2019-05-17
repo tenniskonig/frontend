3cc125c07ff4a8cb5d3a8c5e887546ec1d42b5de
Marvin Kubik <marvin@fam-kubik.de>
2019-05-17 17:46:12 +0200

    Refactor Match class splitted in Subclasses

--- src/app/models/match.ts
+++ src/app/models/match.ts
@@ -4,26 +4,38 @@ export class Match {
   gameID: number;
   player1team1: number;
   player2team2: number;
+  gameDate: Date;
+
+  constructor() {
+  }
+}
+
+export class SingleMatch extends Match {
+  results: string;
+  gameSetTeam1: number;
+  gameSetTeam2: number;
+
+  constructor() {
+    super();
+  }
+}
+
+export class TeamMatch extends Match {
   player3team1: number;
   player4team2: number;
   results: string;
   gameSetTeam1: number;
   gameSetTeam2: number;
-  gameDate: Date;
+
+  constructor() {
+    super();
+  }
+}
+
+export class AdultMatch extends Match {
   playTime: Time;
 
-  constructor(player1team1: number, player2team2: number, results: string, gameDate: Date,
-              gameID?: number, player3team1?: number, player4team2?: number,
-              gameSetTeam1?: number, gameSetTeam2?: number, playTime?: Time) {
-    this.gameID = gameID;
-    this.player1team1 = player1team1;
-    this.player2team2 = player2team2;
-    this.player3team1 = player3team1;
-    this.player4team2 = player4team2;
-    this.results = results;
-    this.gameSetTeam1 = gameSetTeam1;
-    this.gameSetTeam2 = gameSetTeam2;
-    this.gameDate = gameDate;
-    this.playTime = playTime;
+  constructor() {
+    super();
   }
 }
