b014c59cdc48021deaaa87972082c95ce113f066
Tobias Fritz <tobiasfritz@outlook.de>
2019-05-28 16:13:34 +0200

    TEN-123 - Add penalty component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import {RulesComponent} from './rules/rules.component';
 import {MAT_DATE_LOCALE} from '@angular/material';
 import {JwtHttpInterceptor} from './services/httpInterceptor';
 import { RegisterComponent } from './register/register.component';
+import { PenaltyHistoryComponent } from './penalty-history/penalty-history.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { RegisterComponent } from './register/register.component';
     MatchHistoryComponent,
     LoginComponent,
     RulesComponent,
-    RegisterComponent
+    RegisterComponent,
+    PenaltyHistoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/penalty-history/penalty-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-penalty-history',
+  templateUrl: './penalty-history.component.html',
+  styleUrls: ['./penalty-history.component.css']
+})
+export class PenaltyHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
