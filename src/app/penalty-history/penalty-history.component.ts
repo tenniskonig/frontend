1637c1195148d4f60e95b5d98e3cfc412ff62884
Tobias Fritz <tobiasfritz@outlook.de>
2019-05-28 17:47:05 +0200

    TEN-123 - Implement penaltyhistory view

--- src/app/penalty-history/penalty-history.component.ts
+++ src/app/penalty-history/penalty-history.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Penalty} from '../models/penalty';
+import {Observable, of} from 'rxjs';
+import {PenaltyService} from '../services/penalty.service';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-penalty-history',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./penalty-history.component.css']
 })
 export class PenaltyHistoryComponent implements OnInit {
-
-  constructor() { }
+  penalties: Penalty[];
+  penaltiesObservable: Observable<Penalty[]>;
+columns = ['match', 'reason'];
+  constructor(private penaltyService: PenaltyService) {
+  }
 
   ngOnInit() {
+    this.penalties  = [];
+    this.penaltyService.getPenalties(AuthService.currentUserID).subscribe(res => {
+      res.forEach(p => {
+        let penalty = new Penalty();
+        penalty.penaltyID = p.penaltyID;
+        penalty.player = p.player;
+        penalty.match = p.match;
+        penalty.reason = p.reason;
+        this.penalties.push(penalty);
+      });
+      this.penaltiesObservable = of(this.penalties);
+    });
   }
 
 }
